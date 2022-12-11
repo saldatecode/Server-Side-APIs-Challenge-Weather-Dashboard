81e40562111e03ef687a95b86cd0a4ce945ad512
Codeword <saldatejared@gmail.com>
2022-12-11 09:03:15 -0800

    added iamge

--- README.md
+++ README.md
@@ -1,9 +1,176 @@
-# Weather-Dashboard
+<a name="readme-top"></a>
 
-## Discription
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
 
-### Challenge 6 required I create a weather dash board that would provide a 5 day forcast. The user can enter the name of a city, it will then match the weather to the city designated in the search bar.
 
-### If no city is found it will alert the user. Every time the user searches for a city, the data will be stored and can be reused.
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+    <img src="./assets/images/logo.png" alt="Logo" width="80" height="80">
 
-![image](https://user-images.githubusercontent.com/104797672/199342033-5f5e7921-4f9f-45ec-a64c-e804e4e3d1c7.png)
+  <h3 align="center">Weather Dashboard README</h3>
+
+  <p align="center">
+    Welcome to the Weather dashboard README, a great spot to get to know this project!
+    <br />
+    <!-- <a href=""><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="">View Demo</a>
+    ·
+    <a href="">Report Bug</a>
+    ·
+    <a href="\">Request Feature</a> -->
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <!-- <li><a href="#getting-started">Getting Started</a></li> -->
+    <!-- <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li> -->
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+I was tasked to create weather dashboard app so that a user check the weather and get a 5 day forecast for a location of their daily tasks.
+
+I want a user to be able to:
+<br>
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+<br>
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+### Built With
+
+Below are the following technologies and applications utilized to help build the project.<br>
+
+VS Code<br>
+JavaScript<br>
+HTML<br>
+CSS<br>
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+Below you'll be able to see screen shots of the deployed app.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+![Screenshot.png](./assest/images/Screenshot.png)
+
+<!-- USAGE EXAMPLES -->
+<!-- ## Usage
+
+Here're some screenshots from early ideas of the wireframe. See how we went from initial plans to our full-functioning website.
+ -->
+
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ROADMAP -->
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+I strongly feel that contributing well throught and engaging suggestions are instrumental. I am an avid learner and want to let you know any contribution or suggestions are apprecaited.
+
+If you truly believe you can contribute to making this website better, please fork the repo and create a pull request. If you do not wish to fork, you can open and create an issue with the tag "enhancement" or "suggestions". I will then be able to review it and take it into consideration. Lastly, please don't forget to give the project a star!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/newSuggestion`)
+3. Commit your Changes (`git commit -m 'Add some NewSuggestion'`)
+4. Push to the Branch (`git push origin feature/NewSuggestion`)
+5. Open a Pull Request
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Jared Saldate - GitHub [https://github.com/saldatecode]
+
+Project Link: [https://github.com/danpoggetti/work-day-scheduler]
+
+<!-- Demo Link: [] -->
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+
+I want to acknowledge the many great and vast resouces that aided me in the work day scheduler app.
+
+* [Google](https://www.google.com)
+* [YouTube](https://www.youtube.com)
+* [GitHub Pages](https://pages.github.com)
+* [OwaisIslam](https://github.com/OwaisIslam)
+
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors-anon/danpoggetti/work-day-scheduler?style=for-the-badge
+[contributors-url]: https://github.com/danpoggetti/work-day-scheduler/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/danpoggetti/work-day-scheduler?style=for-the-badge
+[forks-url]: https://github.com/danpoggetti/work-day-scheduler/network/members
+[stars-shield]: https://img.shields.io/github/stars/danpoggetti/work-day-scheduler?style=for-the-badge
+[stars-url]: https://github.com/danpoggetti/work-day-scheduler/stargazers
+[issues-shield]: https://img.shields.io/github/issues/danpoggetti/work-day-scheduler?style=for-the-badge
+[issues-url]: https://github.com/danpoggetti/work-day-scheduler/issues
+[license-shield]: https://img.shields.io/github/license/danpoggetti/work-day-scheduler?style=for-the-badge
+[license-url]: https://github.com/danpoggetti/work-day-scheduler/blob/main/LICENSE
+[product-screenshot]: images/screenshot.png
--- assest/js/index.js
+++ assest/js/index.js
@@ -1,125 +1,183 @@
 localStorage.clear();
 
-function findCity() {
-    var cityName = titleCase($("#cityName")[0].value.trim());
-
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=4e12934b1cd60cb4bb4b3d215ad0c35d";
-
-    fetch(apiURL).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-
-                $("#city-name")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
-
-                $("#city-list").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
-
-                localStorage.setItem(cityName, data.coord.lat + " " + data.coord.lon);
-
-                apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=minutely,hourly&units=imperial&appid=4e12934b1cd60cb4bb4b3d215ad0c35d";
-
-                fetch(apiURL).then(function (newResponse) {
-                    if (newResponse.ok) {
-                        newResponse.json().then(function (newData) {
-                            getCurrentWeather(newData);
-                        })
-                    }
-                })
-            })
-        } else {
-            alert("City not found!");
-        }
-    })
-}
-
-// This function gets the info for a city already in the list. It does not need to check whether the city exists as it was already checked when the city was first searched for.
-function getListCity(coordinates) {
-    apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
-
-    fetch(apiURL).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                getCurrentWeather(data);
-            })
-        }
-    })
-}
-
-function getCurrentWeather(data) {
-    $(".results-panel").addClass("visible");
-
-    $("#currentIcon")[0].src = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png";
-    $("#temperature")[0].textContent = "Temperature: " + data.current.temp.toFixed(1) + " \u2109";
-    $("#humidity")[0].textContent = "Humidity: " + data.current.humidity + "% ";
-    $("#wind-speed")[0].textContent = "Wind Speed: " + data.current.wind_speed.toFixed(1) + " MPH";
-    $("#uv-index")[0].textContent = "  " + data.current.uvi;
-
-    if (data.current.uvi < 3) {
-        $("#uv-index").removeClass("moderate severe");
-        $("#uv-index").addClass("favorable");
-    } else if (data.current.uvi < 6) {
-        $("#uv-index").removeClass("favorable severe");
-        $("#uv-index").addClass("moderate");
-    } else {
-        $("#uv-index").removeClass("favorable moderate");
-        $("#uv-index").addClass("severe");
-    }
-
-    getFutureWeather(data);
-}
-
-function getFutureWeather(data) {
-    for (var i = 0; i < 5; i++) {
-        var futureWeather = {
-            date: convertUnixTime(data, i),
-            icon: "http://openweathermap.org/img/wn/" + data.daily[i + 1].weather[0].icon + "@2x.png",
-            temp: data.daily[i + 1].temp.day.toFixed(1),
-            humidity: data.daily[i + 1].humidity
-        }
-
-        var currentSelector = "#day-" + i;
-        $(currentSelector)[0].textContent = futureWeather.date;
-        currentSelector = "#img-" + i;
-        $(currentSelector)[0].src = futureWeather.icon;
-        currentSelector = "#temp-" + i;
-        $(currentSelector)[0].textContent = "Temp: " + futureWeather.temp + " \u2109";
-        currentSelector = "#hum-" + i;
-        $(currentSelector)[0].textContent = "Humidity: " + futureWeather.humidity + "%";
-    }
-}
-
-// This function applies title case to a city name if there is more than one word.
-function titleCase(city) {
-    var updatedCity = city.toLowerCase().split(" ");
-    var returnedCity = "";
-    for (var i = 0; i < updatedCity.length; i++) {
-        updatedCity[i] = updatedCity[i][0].toUpperCase() + updatedCity[i].slice(1);
-        returnedCity += " " + updatedCity[i];
-    }
-    return returnedCity;
-}
-
-// This converts the UNIX time that is received from the server.
-function convertUnixTime(data, index) {
-    const dateObject = new Date(data.daily[index + 1].dt * 1000);
-
-    return (dateObject.toLocaleDateString());
-}
-
 $("#search-button").on("click", function (e) {
-    e.preventDefault();
+    e.preventDefault()
+    var cityName = $("#cityName").val();
+    
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=4e12934b1cd60cb4bb4b3d215ad0c35d";
+    fetch(apiURL)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data)
+        
+        
+        
+        $("#city-name").text(cityName)
+        $("#temperature").text("Temp: "+data.main.temp)
+        $("#humidity").text("Humidity: " + data.main.humidity)
+        $("#wind-speed").text("Wind: " + data.wind.speed)
+       
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=4e12934b1cd60cb4bb4b3d215ad0c35d`)
+        .then(res => res.json())
+    .then(data =>{
+        console.log(data)
+        // 1 new day
+       
+        $("#temp-0").text("Temp: "+data.list[0].main.temp)
+        $("#hum-0").text("Humidity: " + data.list[0].main.humidity)
+        $("#wind-0").text("Wind: " + data.list[0].wind.speed)
+        // 2 new day
+        
+        $("#temp-1").text("Temp: "+data.list[7].main.temp)
+        $("#hum-1").text("Humidity: " + data.list[7].main.humidity)
+        $("#wind-1").text("Wind: " + data.list[7].wind.speed)
+    
+        // 3 new day
+        
+        $("#temp-2").text("Temp: "+data.list[15].main.temp)
+        $("#hum-2").text("Humidity: " + data.list[15].main.humidity)
+        $("#wind-2").text("Wind: " + data.list[15].wind.speed)
+    
+        // 4 new day
+        
+        $("#temp-3").text("Temp: "+data.list[23].main.temp)
+        $("#hum-3").text("Humidity: " + data.list[23].main.humidity)
+        $("#wind-3").text("Wind: " + data.list[23].wind.speed)
+    
+        // 5 new day
+        
+        $("#temp-4").text("Temp: "+data.list[31].main.temp)
+        $("#hum-4").text("Humidity: " + data.list[31].main.humidity)
+        $("#wind-4").text("Wind: " + data.list[31].wind.speed)
 
-    findCity();
+    })
+    })
 
-    $("form")[0].reset();
 })
 
-$(".city-list-box").on("click", ".city-name", function () {
 
-    var coordinates = (localStorage.getItem($(this)[0].textContent)).split(" ");
-    coordinates[0] = parseFloat(coordinates[0]);
-    coordinates[1] = parseFloat(coordinates[1]);
+// localStorage.clear();
 
-    $("#city-name")[0].textContent = $(this)[0].textContent + " (" + moment().format('M/D/YYYY') + ")";
+// function findCity() {
+//     var cityName = titleCase($("#cityName")[0].value.trim());
 
-    getListCity(coordinates);
-})
+//     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=4e12934b1cd60cb4bb4b3d215ad0c35d";
+
+//     fetch(apiURL).then(function (response) {
+//         if (response.ok) {
+//             response.json().then(function (data) {
+
+//                 $("#city-name")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
+
+//                 $("#city-list").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
+
+//                 localStorage.setItem(cityName, data.coord.lat + " " + data.coord.lon);
+
+//                 apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=minutely,hourly&units=imperial&appid=4e12934b1cd60cb4bb4b3d215ad0c35d";
+
+//                 fetch(apiURL).then(function (newResponse) {
+//                     if (newResponse.ok) {
+//                         newResponse.json().then(function (newData) {
+//                             getCurrentWeather(newData);
+//                         })
+//                     }
+//                 })
+//             })
+//         } else {
+//             alert("City not found!");
+//         }
+//     })
+// }
+
+// // This function gets the info for a city already in the list. It does not need to check whether the city exists as it was already checked when the city was first searched for.
+// function getListCity(coordinates) {
+//     apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+
+//     fetch(apiURL).then(function (response) {
+//         if (response.ok) {
+//             response.json().then(function (data) {
+//                 getCurrentWeather(data);
+//             })
+//         }
+//     })
+// }
+
+// function getCurrentWeather(data) {
+//     $(".results-panel").addClass("visible");
+
+//     $("#currentIcon")[0].src = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png";
+//     $("#temperature")[0].textContent = "Temperature: " + data.current.temp.toFixed(1) + " \u2109";
+//     $("#humidity")[0].textContent = "Humidity: " + data.current.humidity + "% ";
+//     $("#wind-speed")[0].textContent = "Wind Speed: " + data.current.wind_speed.toFixed(1) + " MPH";
+//     $("#uv-index")[0].textContent = "  " + data.current.uvi;
+
+//     if (data.current.uvi < 3) {
+//         $("#uv-index").removeClass("moderate severe");
+//         $("#uv-index").addClass("favorable");
+//     } else if (data.current.uvi < 6) {
+//         $("#uv-index").removeClass("favorable severe");
+//         $("#uv-index").addClass("moderate");
+//     } else {
+//         $("#uv-index").removeClass("favorable moderate");
+//         $("#uv-index").addClass("severe");
+//     }
+
+//     getFutureWeather(data);
+// }
+
+// function getFutureWeather(data) {
+//     for (var i = 0; i < 5; i++) {
+//         var futureWeather = {
+//             date: convertUnixTime(data, i),
+//             icon: "http://openweathermap.org/img/wn/" + data.daily[i + 1].weather[0].icon + "@2x.png",
+//             temp: data.daily[i + 1].temp.day.toFixed(1),
+//             humidity: data.daily[i + 1].humidity
+//         }
+
+//         var currentSelector = "#day-" + i;
+//         $(currentSelector)[0].textContent = futureWeather.date;
+//         currentSelector = "#img-" + i;
+//         $(currentSelector)[0].src = futureWeather.icon;
+//         currentSelector = "#temp-" + i;
+//         $(currentSelector)[0].textContent = "Temp: " + futureWeather.temp + " \u2109";
+//         currentSelector = "#hum-" + i;
+//         $(currentSelector)[0].textContent = "Humidity: " + futureWeather.humidity + "%";
+//     }
+// }
+
+// // This function applies title case to a city name if there is more than one word.
+// function titleCase(city) {
+//     var updatedCity = city.toLowerCase().split(" ");
+//     var returnedCity = "";
+//     for (var i = 0; i < updatedCity.length; i++) {
+//         updatedCity[i] = updatedCity[i][0].toUpperCase() + updatedCity[i].slice(1);
+//         returnedCity += " " + updatedCity[i];
+//     }
+//     return returnedCity;
+// }
+
+// // This converts the UNIX time that is received from the server.
+// function convertUnixTime(data, index) {
+//     const dateObject = new Date(data.daily[index + 1].dt * 1000);
+
+//     return (dateObject.toLocaleDateString());
+// }
+
+// $("#search-button").on("click", function (e) {
+//     e.preventDefault();
+
+//     findCity();
+
+//     $("form")[0].reset();
+// })
+
+// $(".city-list-box").on("click", ".city-name", function () {
+
+//     var coordinates = (localStorage.getItem($(this)[0].textContent)).split(" ");
+//     coordinates[0] = parseFloat(coordinates[0]);
+//     coordinates[1] = parseFloat(coordinates[1]);
+
+//     $("#city-name")[0].textContent = $(this)[0].textContent + " (" + moment().format('M/D/YYYY') + ")";
+
+//     getListCity(coordinates);
+// })
